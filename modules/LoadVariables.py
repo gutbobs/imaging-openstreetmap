2f15ceb614453905ce2135ba81320998d6d055d5
Rich Hill <rich@rhill@rm.com>
2016-06-13 13:21:29 +0100

    Moved customisable variables out of code and into an ini file

---
+++ modules/LoadVariables.py
@@ -0,0 +1,45 @@
+import os
+
+__author__ = 'gutbobs'
+
+def converttolist(conversionstring):
+    data=conversionstring.split(',')
+    returnlist=[]
+    for item in data:
+        returnlist.append(float(item))
+    return returnlist
+
+class OpenFile:
+    def __init__(self):
+        self.filename=""
+        self.inputfilename=""
+        self.outputfilename=""
+        self.topleft=[0,0]
+        self.bottomright=[0,0]
+        self.ErrorCode=0
+        self.largestside=1000
+
+    def ReadFile(self):
+        if not os.path.exists(self.filename): self.ErrorCode=1
+
+        if self.ErrorCode==0:
+            variablesdict={}
+            inputfile=open(self.filename,'r')
+            for line in inputfile:
+                data=line.strip().split('=')
+                variable=data[0]
+                value=data[1:]
+                variablesdict[variable]=value
+
+            print variablesdict
+            self.inputfilename="".join(variablesdict['inputfilename'])
+            self.outputfilename="".join(variablesdict['outputfilename'])
+            self.topleft=converttolist("".join(variablesdict['topleft']))
+            self.bottomright=converttolist("".join(variablesdict['bottomright']))
+            self.largestside=int("".join(variablesdict['largestside']))
+
+            if not os.path.exists(self.inputfilename): self.ErrorCode=2
+
+
+
+
